2aab092f3f7b3b0b144cb8447e4afa6009ae937a
Reshetov <vlareshka@mail.ru>
2024-11-07 00:31:12 +0400

    second task almost done

--- LR4/src/main/java/ru/ssau/tk/fruktovi/sandboxoop/operations/TabulatedFunctionOperationService.java
+++ LR4/src/main/java/ru/ssau/tk/fruktovi/sandboxoop/operations/TabulatedFunctionOperationService.java
@@ -6,18 +6,28 @@ import ru.ssau.tk.fruktovi.sandboxoop.functions.TabulatedFunction;
 import ru.ssau.tk.fruktovi.sandboxoop.functions.Point;
 import ru.ssau.tk.fruktovi.sandboxoop.functions.factory.TabulatedFunctionFactory;
 import ru.ssau.tk.fruktovi.sandboxoop.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.fruktovi.sandboxoop.exceptions.InconsistentFunctionsException;
 
 public class TabulatedFunctionOperationService{
-    private final TabulatedFunctionFactory factory;
+    private TabulatedFunctionFactory factory;
 
-    // Конструктор с параметром
     public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
         this.factory = factory;
     }
 
-    // Конструктор без параметров, используется ArrayTabulatedFunctionFactory по умолчанию
     public TabulatedFunctionOperationService() {
         this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public TabulatedFunctionFactory FactoryGetter(){
+        return factory;
+    }
+
+    public void FactorySetter(TabulatedFunctionFactory factory){
+        this.factory = factory;
+    }
+    private interface BiOperation {
+        double apply(double u, double v);
     }
 
 
@@ -33,5 +43,35 @@ public class TabulatedFunctionOperationService{
         }
 
         return points;
+    }
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation) {
+        if (a.getCount() != b.getCount()) {
+            throw new InconsistentFunctionsException("The functions have different number of points.");
+        }
+
+        Point[] pointsA = asPoints(a);
+        Point[] pointsB = asPoints(b);
+        double[] xValues = new double[pointsA.length];
+        double[] yValues = new double[pointsA.length];
+
+        for (int i = 0; i < pointsA.length; i++) {
+            if (pointsA[i].x != pointsB[i].x) {
+                throw new InconsistentFunctionsException("The x-values of the functions do not match.");
+            }
+            xValues[i] = pointsA[i].x;
+            yValues[i] = operation.apply(pointsA[i].y, pointsB[i].y);
+        }
+
+        return factory.create(xValues, yValues);
+    }
+
+    // Метод сложения функций
+    public TabulatedFunction sum(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u + v);
+    }
+
+    // Метод вычитания функций
+    public TabulatedFunction subtract(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u - v);
     }
 }
---
+++ LR4/src/test/java/ru/ssau/tk/fruktovi/sandboxoop/operations/TabulatedFunctionOperationServiceTest.java
@@ -0,0 +1,92 @@
+package ru.ssau.tk.fruktovi.sandboxoop.operations;
+
+import org.junit.jupiter.api.Test;
+import ru.ssau.tk.fruktovi.sandboxoop.functions.ArrayTabulatedFunction;
+import ru.ssau.tk.fruktovi.sandboxoop.functions.Point;
+import ru.ssau.tk.fruktovi.sandboxoop.functions.TabulatedFunction;
+import ru.ssau.tk.fruktovi.sandboxoop.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.fruktovi.sandboxoop.functions.factory.LinkedListTabulatedFunctionFactory;
+import ru.ssau.tk.fruktovi.sandboxoop.functions.factory.TabulatedFunctionFactory;
+import ru.ssau.tk.fruktovi.sandboxoop.operations.TabulatedFunctionOperationService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TabulatedFunctionOperationServiceTest {
+    @Test
+    void testPointsArrayTabulatedFunction() {
+        double[] xArray = {1.0, 2.0, 3.0, 4.0};
+        double[] yArray = {5.0, 6.0, 7.0, 8.0};
+        ArrayTabulatedFunction arrayTabulatedFunction = new ArrayTabulatedFunction(xArray, yArray);
+
+        Point[] points = TabulatedFunctionOperationService.asPoints(arrayTabulatedFunction);
+
+        assertEquals(xArray.length, points.length);
+
+        for (int i = 0; i < xArray.length; i++) {
+            assertEquals(xArray[i], points[i].x, 1e-9);
+            assertEquals(yArray[i], points[i].y, 1e-9);
+        }
+    }
+
+
+
+
+
+    @Test
+    public void asPointsTest() {
+        double[] xValues = {1.0, 2.0, 3.0};
+        double[] yValues = {1.0, 2.0, 3.0};
+
+        ArrayTabulatedFunction function = new ArrayTabulatedFunction(xValues, yValues);
+        Point[] points = TabulatedFunctionOperationService.asPoints(function);
+
+        assertEquals(3, points.length);
+    }
+
+    @Test
+    public void SumTest() {
+        TabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService(factory);
+
+        double[] xValues = {1.0, 2.0, 3.0};
+        double[] yValues1 = {1.0, 2.0, 3.0};
+        double[] yValues2 = {4.0, 5.0, 6.0};
+
+        TabulatedFunction f1 = factory.create(xValues, yValues1);
+        TabulatedFunction f2 = factory.create(xValues, yValues2);
+
+        TabulatedFunction result = service.sum(f1, f2);
+        double[] expectedY = {5.0, 7.0, 9.0};
+
+        for (int i = 0; i < result.getCount(); i++) {
+            assertEquals(expectedY[i], result.getY(i));
+        }
+    }
+
+    @Test
+    public void SubtractTest() {
+        TabulatedFunctionFactory arrayFactory = new ArrayTabulatedFunctionFactory();
+        TabulatedFunctionFactory linkedListFactory = new LinkedListTabulatedFunctionFactory();
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService(arrayFactory);
+
+        double[] xValues = {1.0, 2.0, 3.0};
+        double[] yValues1 = {5.0, 6.0, 7.0};
+        double[] yValues2 = {1.0, 2.0, 3.0};
+
+        TabulatedFunction f1 = arrayFactory.create(xValues, yValues1);
+        TabulatedFunction f2 = linkedListFactory.create(xValues, yValues2);
+
+        TabulatedFunction result = service.subtract(f1, f2);
+        double[] expectedY = {4.0, 4.0, 4.0};
+
+        for (int i = 0; i < result.getCount(); i++) {
+            assertEquals(expectedY[i], result.getY(i));
+        }
+    }
+
+
+
+
+
+
+}
