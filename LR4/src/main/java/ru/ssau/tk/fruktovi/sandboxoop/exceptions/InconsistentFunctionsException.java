ffde72ec58f145e3ccf32598e0302eb58ab4d5d6
Reshetov <vlareshka@mail.ru>
2024-11-07 00:00:36 +0400

    some new changes

---
+++ LR4/src/main/java/ru/ssau/tk/fruktovi/sandboxoop/exceptions/InconsistentFunctionsException.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.fruktovi.sandboxoop.exceptions;
+
+public class InconsistentFunctionsException extends RuntimeException {
+    public InconsistentFunctionsException(){}
+
+    public InconsistentFunctionsException(String message) {
+        super(message);
+    }
+}
--- LR4/src/main/java/ru/ssau/tk/fruktovi/sandboxoop/operations/TabulatedFunctionOperationService.java
+++ LR4/src/main/java/ru/ssau/tk/fruktovi/sandboxoop/operations/TabulatedFunctionOperationService.java
@@ -4,8 +4,24 @@ package ru.ssau.tk.fruktovi.sandboxoop.operations;
 
 import ru.ssau.tk.fruktovi.sandboxoop.functions.TabulatedFunction;
 import ru.ssau.tk.fruktovi.sandboxoop.functions.Point;
+import ru.ssau.tk.fruktovi.sandboxoop.functions.factory.TabulatedFunctionFactory;
+import ru.ssau.tk.fruktovi.sandboxoop.functions.factory.ArrayTabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService{
+    private final TabulatedFunctionFactory factory;
+
+    // Конструктор с параметром
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    // Конструктор без параметров, используется ArrayTabulatedFunctionFactory по умолчанию
+    public TabulatedFunctionOperationService() {
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+
+
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
         int size = tabulatedFunction.getCount();
         Point[] points = new Point[tabulatedFunction.getCount()];
