0003b7dd7aaefeab4a495856b198319a62c1240a
Reshetov <vlareshka@mail.ru>
2024-11-07 21:45:49 +0400

    operators added

---
+++ LR4/src/main/java/ru/ssau/tk/fruktovi/sandboxoop/operations/LeftSteppingDifferentialOperator.java
@@ -0,0 +1,25 @@
+package ru.ssau.tk.fruktovi.sandboxoop.operations;
+
+import ru.ssau.tk.fruktovi.sandboxoop.functions.MathFunction;
+
+public class LeftSteppingDifferentialOperator extends SteppingDifferentialOperator{
+
+    public LeftSteppingDifferentialOperator(double step) {
+        super(step);
+    }
+
+    @Override
+    public MathFunction derive(MathFunction function) {
+        return new MathFunction() {
+            @Override
+            public double apply(double x) {
+                return (function.apply(x) - function.apply(x - step)) / step;
+            }
+        };
+    }
+
+    @Override
+    public double apply(double x) {
+        throw new UnsupportedOperationException();
+    }
+}
