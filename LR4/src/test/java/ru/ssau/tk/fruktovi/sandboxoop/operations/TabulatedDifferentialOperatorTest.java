c26a80230c53d3ede79e7fa57ec803b65a80591d
Gridyaev <gr.oleg.721@gmail.com>
2024-11-07 22:21:26 +0400

    sub and mult

--- LR4/src/test/java/ru/ssau/tk/fruktovi/sandboxoop/operations/TabulatedDifferentialOperatorTest.java
+++ LR4/src/test/java/ru/ssau/tk/fruktovi/sandboxoop/operations/TabulatedDifferentialOperatorTest.java
@@ -30,32 +30,4 @@ class TabulatedDifferentialOperatorTest {
         assertInstanceOf(LinkedListTabulatedFunctionFactory.class, operator.getFactory());
     }
 
-    @Test
-    void testDeriveLinkedList(){
-        TabulatedDifferentialOperator operator = new TabulatedDifferentialOperator(new LinkedListTabulatedFunctionFactory());
-        TabulatedFunction function = operator.derive(new LinkedListTabulatedFunction(x -> Math.pow(x,2) + x + 1, -2, 1, 4));
-
-        double[] xValues = new double[] {-2.0, -1.25, -0.5, 0.25};
-        double[] yValues = new double[] {-2.25, -0.75, 0.75, 0.75};
-
-        for (int i = 0; i < function.getCount(); i++) {
-            assertEquals(xValues[i], function.getX(i));
-            assertEquals(yValues[i], function.getY(i));
-        }
-    }
-
-    @Test
-    void testDeriveWithNewArrayFunction() {
-        TabulatedDifferentialOperator operator = new TabulatedDifferentialOperator(new ArrayTabulatedFunctionFactory());
-        TabulatedFunction function = operator.derive(new ArrayTabulatedFunction(x -> Math.pow(x, 2) + x + 1, -2, 1, 4));
-
-        // Ожидаемые значения на основе пересчитанных производных
-        double[] xValues = new double[] {-2.0, -1.25, -0.5, 0.25};
-        double[] yValues = new double[] {-3.0, -1.5, 0.0, 1.5};
-
-        for (int i = 0; i < function.getCount(); i++) {
-            assertEquals(xValues[i], function.getX(i), 1e-9);
-            assertEquals(yValues[i], function.getY(i), 1e-9);
-        }
-    }
 }
