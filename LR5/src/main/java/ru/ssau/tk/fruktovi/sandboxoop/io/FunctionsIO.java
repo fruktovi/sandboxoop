2b3dd911478498e2d8c3d1ea4fee8c92854fff81
Reshetov <vlareshka@mail.ru>
2024-11-12 22:43:07 +0400

    some changes

--- LR5/src/main/java/ru/ssau/tk/fruktovi/sandboxoop/io/FunctionsIO.java
+++ LR5/src/main/java/ru/ssau/tk/fruktovi/sandboxoop/io/FunctionsIO.java
@@ -1,4 +1,28 @@
 package ru.ssau.tk.fruktovi.sandboxoop.io;
+import ru.ssau.tk.fruktovi.sandboxoop.functions.Point;
+import ru.ssau.tk.fruktovi.sandboxoop.functions.TabulatedFunction;
 
-public class FunctionsIO {
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public final class FunctionsIO {
+    private FunctionsIO() {
+        throw new UnsupportedOperationException("Instances of FunctionsIO are not allowed.");
+    }
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function){
+        PrintWriter file = new PrintWriter(writer);
+
+        int count = function.getCount();
+
+        file.println(count);
+
+        for(Point point : function){
+            double x = point.x;
+            double y = point.y;
+            file.printf("%f %f\n",x,y);
+        }
+        file.flush();
+    }
 }
--- LR5/src/main/java/ru/ssau/tk/fruktovi/sandboxoop/io/TabulatedFunctionFileWriter.java
+++ LR5/src/main/java/ru/ssau/tk/fruktovi/sandboxoop/io/TabulatedFunctionFileWriter.java
@@ -1,4 +1,30 @@
 package ru.ssau.tk.fruktovi.sandboxoop.io;
 
-public class TabulatedFunctionFileWriter {
-}
+import ru.ssau.tk.fruktovi.sandboxoop.functions.ArrayTabulatedFunction;
+import ru.ssau.tk.fruktovi.sandboxoop.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.fruktovi.sandboxoop.functions.SqrFunction;
+import ru.ssau.tk.fruktovi.sandboxoop.functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+    public class TabulatedFunctionFileWriter {
+        public static void main(String[] args){
+            String arrayFunctionLinePath = "output/array function.txt";
+            String linkedListFunctionLinePath = "output/linked list function.txt";
+
+            try(
+                    BufferedWriter arrayWrite = new BufferedWriter(new FileWriter(arrayFunctionLinePath));
+                    BufferedWriter linkedWrite = new BufferedWriter(new FileWriter(linkedListFunctionLinePath));
+            ) {
+                TabulatedFunction arrayFunction = new ArrayTabulatedFunction(new SqrFunction(), 0, 10, 5);
+                TabulatedFunction linkedListFunction = new LinkedListTabulatedFunction(new double[]{1,2,3}, new double[]{1, 4, 9});
+
+                FunctionsIO.writeTabulatedFunction(arrayWrite, arrayFunction);
+                FunctionsIO.writeTabulatedFunction(linkedWrite, linkedListFunction);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
