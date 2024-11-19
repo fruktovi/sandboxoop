77df265d461eee7295e037c04a21518e1ff51761
Gridyaev <gr.oleg.721@gmail.com>
2024-11-19 18:13:37 +0400

    read and write 1 and 2 x task

--- LR5/src/main/java/ru/ssau/tk/fruktovi/sandboxoop/concurrent/WriteTask.java
+++ LR5/src/main/java/ru/ssau/tk/fruktovi/sandboxoop/concurrent/WriteTask.java
@@ -2,21 +2,23 @@ package ru.ssau.tk.fruktovi.sandboxoop.concurrent;
 
 import ru.ssau.tk.fruktovi.sandboxoop.functions.TabulatedFunction;
 
-public class ReadTask implements Runnable {
+public class WriteTask implements Runnable {
 
     private final TabulatedFunction function;
+    private final double value;
 
-    public ReadTask(TabulatedFunction function) {
+    public WriteTask(TabulatedFunction function, double value) {
         this.function = function;
+        this.value = value;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < function.getCount(); ++i) {
             synchronized (function) {
-                System.out.printf("After read: i = %d, x = %f, y = %f\n", i, function.getX(i), function.getY(i));
+                function.setY(i, value);
+                System.out.printf("Writing for index %d complete\n", i);
             }
         }
     }
-
 }
